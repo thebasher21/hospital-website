336800ad72367763ec29d75d96396e0eab97d5ce
thebasher21 <vihaan1303@gmail.com>
2025-03-23 20:48:26 +0530

    Update Next.js configuration to set basePath and assetPrefix to empty for non-production environments, and add "type": "module" to package.json for ES module support.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   trailingSlash: true,
   images: {
     remotePatterns: [
